ef9f8358109e51f19d681ae98c2015abe36666c4
fabby <ashtonfabbt@gmail.com>
2019-03-08 15:27:59 +0200

    first commit

---
+++ dependancies/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.typed', {
+    strings: ['ashtonFabby', 'Farai Bauti', 'codeJunkie'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    backDelay: 500,
+    cursorChar: '',
+    
+  });
+
+  
